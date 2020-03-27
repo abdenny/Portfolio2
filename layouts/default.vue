9a64a267a7941985ffca40581beaabc4aa81c61e
Austin Denny <austinbdenny@gmail.com>
2020-03-27 17:59:58 -0500

    Initial vue site

---
+++ layouts/default.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- Main -->
+  <main>
+    <!-- Content -->
+    <nuxt />
+
+    <!-- Footer -->
+    <footer class="footer">
+      | Crafted with minimialism &
+      functionailty first|
+    </footer>
+  </main>
+</template>
+
+<style>
+/* Block Elements */
+* {
+  text-decoration: none;
+  box-sizing: border-box;
+  margin: 0;
+}
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  color: #494b4b;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  overflow-x: hidden;
+  user-select: unset;
+}
+a {
+  font-weight: bold;
+  color: #494b4b;
+  text-decoration: none;
+}
+hr {
+  width: 100%;
+  border-width: 0.5px;
+  border-color: #e0e0e0;
+  margin: 16px 0;
+}
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+}
+
+.footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 13px;
+  height: 56px;
+  width: 100%;
+  z-index: 3;
+}
+.footer__icon {
+  margin: 0 4px;
+}
+.footer__link {
+  margin: 0 4px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,247 @@
+<template>
+  <!-- Content -->
+  <div class="content">
+    <!-- Profile -->
+    <img class="content__profile" src="/img/me.jpg" />
+    <article
+      class="content__article content__article--center content__article--margin-16"
+    >
+      <p class="content__article-title content__article-title--large">
+        Austin Denny
+      </p>
+      <p class="content__article-subtitle">
+        Web Developer
+      </p>
+      <p class="content__article-links">
+        <a
+          href="https://resumepdfad.s3.us-east-2.amazonaws.com/AustinDennyAtsResume.pdf"
+          target="_blank"
+        >
+          <i class="far fa-file-alt content__article-link" />
+        </a>
+        <a href="https://github.com/abdenny" target="_blank">
+          <i class="fab fa-github content__article-link" />
+        </a>
+        <a
+          href="https://linkedin.com/in/austin-denny-0b77a8197/"
+          target="_blank"
+        >
+          <i class="fab fa-linkedin-in content__article-link" />
+        </a>
+        <a href="mailto:austinbdenny@gmail.com" target="_blank">
+          <i class="far fa-envelope content__article-link" />
+        </a>
+      </p>
+    </article>
+
+    <!-- About Me -->
+    <article class="content__article content__article--margin-16">
+      <p class="content__article-details">
+        Lorem ipsum dolor sit amet, facilisi nec,
+        <a href="#" target="_blank">nonummy</a> hendrerit elit a metus
+        <a href="#" target="_blank">ultricies</a>, ante tortor volutpat
+        suspendisse eos hac in, metus quis in felis egestas, sapien id id eu
+        duis volutpat. <br /><br />
+        A turpis magna ac, eu etiam
+        <a href="#" target="_blank">tellus rutrum</a> enim arcu eget. Non dolor
+        eu torquent. <a href="#" target="_blank">Aenean nostra</a> torquent
+        fringilla, ac wisi penatibus id sit enim scelerisque, metus ac erat, vel
+        id.
+      </p>
+    </article>
+
+    <!-- Projects -->
+    <article class="content__article content__article--margin-24">
+      <p class="content__article-title content__article-title--medium">
+        <i class="fa fa-briefcase" /> Projects
+      </p>
+      <hr />
+    </article>
+
+    <!-- Project -->
+    <article class="content__article content__article--margin-8">
+      <!-- Title -->
+      <p class="content__article-title content__article-title">
+        Tribuo
+      </p>
+      <!-- Details -->
+      <p class="content__article-details content__article--margin-8">
+        Tribuo is a full-stack web app for company-wide task management that
+        tracks employees, departments, and tasks in a relational database.<br />
+        <span class="content__article-metadata">
+          <b><i class="fa fa-code" /> Technology:</b> HTML, CSS, JavaScript ES6,
+          AJAX, Node.js, Express, PostgreSQL, Sequelize, EJS, Hasura, Heroku,
+          and GraphQL. Deployed on AWS (RDS & EC2).
+        </span>
+      </p>
+      <!-- Preview -->
+      <a
+        href="http://ec2-18-222-118-237.us-east-2.compute.amazonaws.com"
+        target="_blank"
+      >
+        <img src="/img/tribuo.png" class="content__article-preview" />
+      </a>
+      <!-- Links -->
+      <p class="content__article-details">
+        -- <br />
+        <a
+          href="http://ec2-18-222-118-237.us-east-2.compute.amazonaws.com"
+          target="_blank"
+          >Demo</a
+        >
+        |
+        <a href="https://github.com/abdenny/Tribuo" target="_blank"
+          >View on Github</a
+        >
+      </p>
+      <hr />
+    </article>
+
+    <!-- Project -->
+    <article class="content__article content__article--margin-8">
+      <!-- Title -->
+      <p class="content__article-title content__article-title">
+        Book•Smart
+      </p>
+      <!-- Details -->
+      <p class="content__article-details content__article--margin-8">
+        Book•Smart is a front-end API-driven search engine for finding books and
+        viewing both historical and current bestseller data from The New York
+        Times.<br />
+        <span class="content__article-metadata">
+          <b><i class="fa fa-code" /> Technology:</b> HTML, CSS, Javascript
+        </span>
+      </p>
+      <!-- Preview -->
+      <a
+        href="http://jayesolutions.net.s3-website.us-east-2.amazonaws.com/index.html"
+        target="_blank"
+      >
+        <img src="/img/homepage.png" class="content__article-preview" />
+      </a>
+      <!-- Links -->
+      <p class="content__article-details">
+        -- <br />
+        <a
+          href="http://jayesolutions.net.s3-website.us-east-2.amazonaws.com/index.html"
+          target="_blank"
+          >Demo</a
+        >
+        |
+        <a href="https://github.com/abdenny/Book_Smart" target="_blank"
+          >View on Github</a
+        >
+      </p>
+      <hr />
+    </article>
+
+    <!-- Project -->
+    <article class="content__article content__article--margin-8">
+      <!-- Title -->
+      <p class="content__article-title content__article-title">
+        Title
+      </p>
+      <!-- Details -->
+      <p class="content__article-details content__article--margin-8">
+        Blackjack is a front-end app that allows users to play blackjack against
+        the dealer, complete with full game logic.<br />
+        <span class="content__article-metadata">
+          <b><i class="fa fa-code" /> Technology:</b> HTML, CSS, Javascript
+        </span>
+      </p>
+      <!-- Preview -->
+      <a href="https://abdenny.github.io/Blackjack" target="_blank">
+        <img src="/img/blackjack.png" class="content__article-preview" />
+      </a>
+      <!-- Links -->
+      <p class="content__article-details">
+        -- <br />
+        <a href="https://abdenny.github.io/Blackjack" target="_blank">Demo</a> |
+        <a href="https://github.com/abdenny/Blackjack" target="_blank"
+          >View on Github</a
+        >
+      </p>
+      <hr />
+    </article>
+  </div>
+</template>
+
+<style>
+/* Block Elements */
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  z-index: 4;
+}
+.content__profile {
+  height: 180px;
+  width: 180px;
+  border: solid white 4px;
+  border-radius: 50%;
+  box-shadow: 2px 2px 8px #494b4b;
+}
+.content__article {
+  width: 100%;
+}
+.content__article-title {
+  font-size: 22px;
+  font-weight: bold;
+  color: #3a4145;
+}
+.content__article-subtitle {
+  font-size: 22px;
+  font-weight: bold;
+  color: #a9a9a9;
+}
+.content__article-details {
+  font-size: 16px;
+}
+.content__article-metadata {
+  font-size: 13px;
+}
+.content__article-links {
+  margin: 8px 0;
+}
+.content__article-link {
+  font-size: 22px;
+  margin: 0 8px;
+}
+.content__article-preview {
+  width: 100%;
+  max-width: 800px;
+  margin-top: 16px;
+}
+
+/* Modifiers */
+.content__article-title--medium {
+  font-size: 32px;
+  font-weight: bold;
+}
+.content__article-title--large {
+  font-size: 40px;
+  font-weight: bold;
+}
+.content__article--center {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.content__article--margin-8 {
+  margin-top: 8px;
+}
+.content__article--margin-16 {
+  margin-top: 16px;
+}
+.content__article--margin-24 {
+  margin-top: 24px;
+}
+
+@media only screen and (min-width: 800px) {
+  .content {
+    width: 800px;
+  }
+}
+</style>
